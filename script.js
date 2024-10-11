a2e106224026106920fcd2c6b969eaa99518153d
OmegaTroy <juli.mjoi44@gmail.com>
2024-10-11 01:55:56 -0300

    add heart mouse move

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 onload = () => {
     document.body.classList.remove("container");
   };
+
+  document.addEventListener("mousemove", function (e) {
+    let body = document.querySelector("body");
+    let heart = document.createElement("span");
+  
+    let size = Math.random() * 40;
+    let transformValue = Math.random() * 360;
+  
+    heart.style.left = e.offsetX + "px";
+    heart.style.top = e.offsetY + "px";
+    heart.style.fontSize = 10 + size + "px";
+    heart.style.transform = "rotate(" + transformValue + "deg)";
+  
+    body.appendChild(heart);
+  
+    setTimeout(() => {
+      heart.remove();
+    }, 1000);
+  });
